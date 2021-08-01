85ee55d52a4bc3ecb29ea6d8009155415e58cf05
Rrahul Raja <rahulraja.ngp@gmail.com>
2021-08-01 13:27:57 +0530

    Added code for Blockchain

---
+++ blockchain/Blockchain.js
@@ -0,0 +1,86 @@
+const Block = require('./Block');
+const axios = require('axios');
+
+class Blockchain {
+  constructor() {
+    this.blockchain = [Block.generateGenesisBlock()];
+    this.transactions = [];
+  }
+
+  get() {
+    return this.blockchain;
+  }
+
+  createTransaction(transactionInfo) {
+    this.transactions.push(transactionInfo);
+  }
+
+  mineNewBlock(minerAddress) {
+    if (!this.transactions.length) {
+      return null;
+    }
+
+    const lastBlock = this.blockchain[this.blockchain.length - 1];
+
+    const proof = this.proofOfWork(lastBlock.data.proofOfWork);
+
+    this.createTransaction({
+      from: 'network',
+      to: minerAddress,
+      amount: 1,
+      createdAt: Date.now(),
+    });
+
+    const newBlock = Block.generateNextBlock(lastBlock, {
+      transactions: this.transactions,
+      proofOfWork: proof,
+    });
+
+    this.transactions = [];
+
+    this.blockchain.push(newBlock);
+
+    return newBlock;
+  }
+
+  async consensus() {
+      const otherChains = await this.discoverPeerChains()
+
+      let longestChain = this.blockchain
+
+      for(let chain in otherChains) {
+          if(chain.length > longestChain.length) {
+              longestChain = chain
+          }
+      }
+
+      this.blockchain = longestChain
+  }
+
+
+  async discoverPeerChains() {
+      const peerChains = []
+
+      const peersList = process.env.PEERS.split(',')
+
+      for(let peerUrl of peersList) {
+          const peerChain = await axios.get(`${peerUrl}/api/blocks`)
+
+          peerChains.push(peerChain)
+      }
+
+      return peerChains
+  }
+
+  proofOfWork(lastProof) {
+      let incrementor = lastProof + 1
+
+      while(!(incrementor % 9 === 0 && incrementor % lastProof === 0)) {
+          incrementor += 1
+      }
+
+      return incrementor
+  }
+}
+
+module.exports = Blockchain
